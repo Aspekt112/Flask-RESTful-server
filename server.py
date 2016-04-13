3e1e834ed4494f0e15ddf2f214443a6b4f4b1aa3
Nurmatov <atnurmatov@gmail.com>
2016-04-13 18:21:33 +0300

    In spite of everything, it works!

---
+++ server.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, jsonify, abort
+from datetime import datetime
+
+app = Flask(__name__)
+
+data = {
+    'hello': 'Hello World!',
+    'name': 'My name is Flask Server',
+}
+
+@app.route('/')
+def render_index():
+    return render_template('index.html')
+
+@app.route('/dictionary', methods=['POST'])
+def post_func():
+    if 'key' in request.json and 'value' in request.json:
+        key = request.json['key']
+        value = request.json['value']
+        if data.__contains__(key):
+            return abort(409)
+        node = {key: value}
+        data.update(node)
+        return jsonify(success=True, result=data)
+    else:
+        return abort(400)
+
+@app.route('/dictionary', methods=['PUT'])
+def put_func():
+    if 'key' in request.json and 'value' in request.json:
+        key = request.json['key']
+        value = request.json['value']
+        if not data.__contains__(key):
+            return abort(404)
+        if data[key] == value:
+            return abort(409)
+        node = {key: value}
+        data.update(node)
+        return jsonify(success=True, result=data)
+    else:
+        return abort(404)
+
+@app.route('/dictionary/<key>', methods=['DELETE'])
+def delete_func(key):
+    if data.__contains__(key):
+        if data.pop(key):
+            time = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M")
+            response = "{\"%s\": \"null\", \"time\": \"%s\"}" % (key, time)
+            return jsonify(success=True, result=response)
+        else:
+            return jsonify(success=True, result='200')
+    else:
+        return jsonify(success=True, result='200')
+
+@app.route('/dictionary/<key>',methods=['GET'])
+def get_func(key):
+    if data.__contains__(key):
+        response = data[key]
+        return jsonify(success=True, result=response)
+    else:
+        return abort(404)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
